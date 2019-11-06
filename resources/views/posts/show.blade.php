ef4254ea08b7e1ff349532dbdc0bc84b4faebeb3
Andrey <web.andrew@yandex.ru>
2019-11-06 15:24:10 +0300

    updated post view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,9 +7,30 @@
                 <img src="/storage/{{ $post->image }}" alt="{{ $post->caption }}">
             </div>
             <div class="col-4">
+                <div class="d-flex align-items-center pb-3">
+                    <div class="col-3 pr-3">
+                        <a href="/profile/{{ $post->user->id }}">
+                            <img class="rounded-circle" src="/storage/{{ $post->user->profile->image }}" alt="{{ $post->user->profile->description }}">
+                        </a>
+                    </div>
+                    <div class="col-9">
+                        <h5 class="font-weight-bold">
+                            <a class="text-dark" href="/profile/{{ $post->user->id }}">{{ $post->user->username }}</a>
+                        </h5>
+                    </div>
+                </div>
+
+                <hr>
+
                 <div>
-                    <h3>{{ $post->user->username }}</h3>
-                    <p>{{ $post->caption }}</p>
+                    <p>
+                        <span class="font-weight-bold">
+                            <a class="text-dark" href="/profile/{{ $post->user->id }}">
+                                {{ $post->user->username }}
+                            </a>
+                        </span>
+                        {{ $post->caption }}
+                    </p>
                 </div>
             </div>
         </div>
